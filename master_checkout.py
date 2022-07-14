4301a83d34c0fe8884872eca531961cea37dda65
Peter Kentish De La Iglesia <peter.kentish@travelperk.com>
2022-07-14 09:40:50 +0200

    APP-56896 modify amount of polls and delay between them

--- master_checkout.py
+++ master_checkout.py
@@ -1,4 +1,4 @@
-# master_checkout.py is designed to check out and pull from master all of your repositories.
+# master_checkout.py is designed to check out and pull from master (or main, see later) all of your repositories.
 # It works on the assumption that it lives alongside your git repos, something like this:
 # workspace/
 #   ├─ backend/
@@ -7,12 +7,23 @@
 #   ├─ tda_amadeus/
 #   ├─ master_checkout.py
 
-# if this applies, simply run the file and it will checkout all of your projects. If there
+# However it can also be called from anywhere if the first parameter you pass it is the directory
+# where you projects live. 
+# Simply run the file and it will checkout all of your projects. If there
 # are any problems, it will report back the projects that failed so you can check if you 
-# had any leftover code sitting about. 
+# had any leftover code or commits leftover. 
+# If you have any projects that have a primary branch as `main` instead of `master` , add them as strings
+# to the following constant:
+PROJECTS_WITH_MAIN_BRANCH = ['tools']
+
+# USAGE (in top level directory)
+# python master_checkout.py
+# OR (from anywhere)
+# python master_checkout.py ~/workspace/
 import os
 import subprocess
-
+from pathlib import Path
+import sys
 
 class bcolors:
     HEADER = '\033[95m'
@@ -25,13 +36,16 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def checkout_master():
-    checkout_master_process =  subprocess.Popen(['git', 'checkout', 'master'], 
+
+def checkout_primary(project_name):
+    primary_branch = 'master' if project_name not in PROJECTS_WITH_MAIN_BRANCH else 'main'
+    checkout_master_process =  subprocess.Popen(['git', 'checkout', primary_branch], 
                            stdout=subprocess.PIPE,
                            universal_newlines=True)
     checkout_master_process.communicate()
     return checkout_master_process.returncode
     
+
 def git_pull():
     pull_process = subprocess.Popen(['git', 'pull'], 
                            stdout=subprocess.PIPE,
@@ -40,16 +54,28 @@ def git_pull():
     return pull_process.returncode
 
 
-working_dir = os.getcwd()
-all_current_directories = os.listdir(working_dir)
+if (len(sys.argv) > 1):
+    first_arg = sys.argv[1]
+    path_to_repos = Path(first_arg)
+    if os.path.isdir(path_to_repos):
+        working_dir = path_to_repos
+    else:
+        print(f"{bcolors.FAIL} It looks like the supplied path '{path_to_repos}' is not a proper directory{bcolors.ENDC}")
+        sys.exit()
+else:
+    working_dir = os.getcwd()
 
+os.chdir(working_dir)
+
+all_current_directories = os.listdir(working_dir)
 directories_with_failures=[]
+
 for directory in all_current_directories:
     if os.path.isdir(directory) and not directory.startswith('.'):
         print(f"{bcolors.HEADER}{bcolors.BOLD}------------------------- {directory.upper()} ------------------------- {bcolors.ENDC}")
         
         os.chdir(directory)
-        master_checkout_return_code = checkout_master()
+        master_checkout_return_code = checkout_primary(directory)
         pull_from_origin_return_code = git_pull()    
         
         if master_checkout_return_code != 0 or pull_from_origin_return_code != 0:
